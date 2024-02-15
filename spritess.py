7c27fd194c3efcdd17aca96039cb3c1e1b316107
Diana12345r <shagalieva.d@gmail.com>
2024-02-15 21:39:50 +0300

    first

---
+++ spritess.py
@@ -0,0 +1,33 @@
+import pygame
+import random
+
+
+class Ufo(pygame.sprite.Sprite):
+    def __init__(self, filename, group):
+        super().__init__()
+        self.coo_x = random.randint(0, 800)
+        self.image = pygame.image.load(filename).convert_alpha()
+        self.rect = self.image.get_rect(center=(self.coo_x, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.add(group)
+
+    def update(self):
+        if self.rect.y < 533:
+            self.rect.y += 5
+        else:
+            self.kill()
+
+
+class Plane(pygame.sprite.Sprite):
+    def __init__(self, filename, coo_x, coo_y, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.plane_image = pygame.image.load(filename).convert_alpha()
+        self.rect = self.plane_image.get_rect(center=(coo_x, coo_y))
+        self.mask = pygame.mask.from_surface(self.plane_image)
+        self.speed = speed
+
+    def update(self, posi):
+        if posi == 'left' and 0 < self.rect.x:
+            self.rect.x -= self.speed
+        elif posi == 'right' and self.rect.x < 780:
+            self.rect.x += self.speed
